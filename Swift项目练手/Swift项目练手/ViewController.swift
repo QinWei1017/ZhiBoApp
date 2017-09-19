bafe511269972df7e06d70c59531163158723d41
QinWei <qinwei@qinweidembp>
2017-09-19 20:18:38 +0800

    初始化项目

---
+++ Swift项目练手/Swift项目练手/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift项目练手
+//
+//  Created by QinWei on 2017/9/19.
+//  Copyright © 2017年 QinWei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
